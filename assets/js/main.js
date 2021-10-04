3f4df9ff9ce695e13e54a6973e52b19ad9d0cc08
umqambi <50770856+umqambi@users.noreply.github.com>
2021-10-04 17:23:14 +0300

    fix lang tab
    fix lang tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-
 	$('.menu-toggle').click(function(){
 		$('#top-nav').slideToggle(300);
         $('#header').slideToggle(0);
         $('#main').toggleClass("muted");
 	});
+});
 
-
+$(document).ready(function(){
+	$('.lang-toggle').click(function(){
+        $(this).parent().find('#lang-content').slideToggle(300);
+        $(this).find($('.btn-icon')).toggleClass("rotated");
+    });
+    
 });
