0a39107b40a6a3af704b43b9c0bd76fcd7ddeddf
umqambi <50770856+umqambi@users.noreply.github.com>
2021-10-04 22:59:28 +0300

    some fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,29 @@ $(document).ready(function(){
     });
     
 });
+
+$('.reviews-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
